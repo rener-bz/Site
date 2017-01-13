083bf189f496260e74a476bf4056a5164586b09b
rener-bz <rener.bezerra@gmail.com>
2017-01-13 11:27:46 -0200

    estruturacao

--- theme/js/main.js
+++ theme/js/main.js
@@ -7,6 +7,13 @@ $('.hamburger').on('click touchstart', function(e){
     e.preventDefault();
     });
 });
+//    Altura do site
+//    var altSite = $(window).height();
+//    Altura do site menos topo
+//    altSite = altSite - $('.content-title').height();
+//    altSite = altSite - $('.content-social').height();
+//    $('.conteudo').css({"min-height":+altSite+"px"});
+
 //$(".main-title").typed({
 //    strings: ["Desenvolvendo websites para pessoas.","Desenvolvendo lojas virtuais para pessoas.", "Desenvolvendo soluções para pessoas"],
 //    typeSpeed: 0
